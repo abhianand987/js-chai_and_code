d5103e678e16f505c2dd3961295f2c2084643584
Abhishek Anand <87860680+abhianand987@users.noreply.github.com>
2024-05-01 20:21:01 +0000

    FUNCTION CONFIG DONE

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,3 +29,34 @@ function loginUserMessage(username){
 console.log(loginUserMessage('Abhishek'))
 
 //loginUserMessage() undefined just logged in
+
+function calculateCartPrice(...num1){  // ... is rest operator as well as spread operator to combine objects + arrays
+     return num1
+}
+
+// ... rest operator is used when we dont know how many parameters we have and it returns an array
+
+console.log(calculateCartPrice(100,200,400)) //bcoz of ... it will return an array -->[100,200,400] else it will just return the first argument (100)
+
+const user = {
+    username : 'abhishek',
+    prices : 100
+}
+
+function handleObject(anyObject){
+    console.log(`user name is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username : 'sam',
+    price : 19998
+})
+
+const myArr = [200 , 300 , 400]
+
+function returnSecondValue(arr){
+    return arr[1];
+}
+
+console.log(returnSecondValue(myArr));
