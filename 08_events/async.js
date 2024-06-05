e5cb9d6e3c46b2e101d206b2b7cb114511535f3a
Abhishek Anand <87860680+abhianand987@users.noreply.github.com>
2024-06-05 19:18:45 +0000

    config done

---
+++ 08_events/async.js
@@ -0,0 +1,19 @@
+// async code is nothing high fi
+//JS asynchronous language h but uska default behaviour synchronous hota h. Us behavior ko hame forcefully bta kr async bnana pdta h. 
+//Sbse easy example h aapke settimeout or setinterval methods. Hope aur clear ho gya h ab.
+// Default JS --> SYNCHRONOUS(order by order code is executed)
+//            --> SINGLE-THREADED by default
+
+//Execution Context
+   // --execute one line of code @ a time
+   // single threaded --> Each operation waits for the last one to complete before executing
+
+// blocking code vs non blocking code
+// bc --> block the flow of program --> read file sync
+// & nbc --> does not block execution --> read file async
+
+// nbc is not always prefereed and it depends on the use-cases
+// blocking code --> jb tak mere database me entry ho kar mujhe response nhi mil jaiye tab tak ruko and 
+// phir user ko message do ki registration successful
+
+
